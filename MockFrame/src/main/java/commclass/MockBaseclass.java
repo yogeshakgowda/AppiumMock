13fd237923b8cd5282d49b3407b590a20f8f4c88
yogeshakgowda <yogesha.052k@gmail.com>
2024-06-06 18:51:38 +0530

    update

--- MockFrame/src/main/java/commclass/MockBaseclass.java
+++ MockFrame/src/main/java/commclass/MockBaseclass.java
@@ -1,14 +1,19 @@
 package commclass;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-
+import java.time.Duration;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
 
 
 public class MockBaseclass 
@@ -19,7 +24,17 @@ public class MockBaseclass
     public AndroidDriverUtils Adutlis;
     public FileUtility futlis=new FileUtility();
     public PomPage.flipkartjava flip;
- 
+    public AppiumDriverLocalService service;
+    
+    @BeforeSuite
+    public void startServer()
+    {
+    	File f= new File("C:\\Users\\HP\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js");
+		service=new AppiumServiceBuilder().withAppiumJS(f).withIPAddress("127.0.0.1")
+				.usingPort(4723).withTimeout(Duration.ofSeconds(300)).build();
+		
+		service.start();
+    }
     @BeforeClass
     public void startApp() throws IOException
     {
@@ -58,6 +73,13 @@ public class MockBaseclass
     	
     	driver.quit();
     	
+    }
+    
+	
+    @AfterSuite
+    public void closeServer()
+    {
+    	service.stop();
     }
    
 }
